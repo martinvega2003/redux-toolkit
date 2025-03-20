051467801bc785f74179712f7b78b3b6eb48d145
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-20 15:19:35 -0300

    Select the taskState in the App.jsx and show the initial value in console

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { store } from './app/store' // Import the store created
-import { Provider } from 'react-redux' // Import provider from redux
+import { useSelector, useDispatch } from 'react-redux' // useSelector is used to select values from the state, useDispatch is used to execute actions on the state
 
 const App = () => {
+  const taskState = useSelector(state => state.tasks) // tasks is the name assigned to taskReducer in store.js
+  console.log(taskState)
   return ( // Wrap the App with the redux Provider and pass the store as prop
-    <Provider store={store}>
-      <div className='text-center text-9xl text-red-500'>App</div>
-    </Provider>
+    <div className='text-center text-9xl text-red-500'>App</div>
   )
 }
 
