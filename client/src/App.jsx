04c47f905fab298334aa6b3092c11f62cdd2181c
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-20 14:56:33 -0300

    Setup the redux store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { store } from './app/store' // Import the store created
+import { Provider } from 'react-redux' // Import provider from redux
 
 const App = () => {
-  return (
-    <div className='text-center text-9xl text-red-500'>App</div>
+  return ( // Wrap the App with the redux Provider and pass the store as prop
+    <Provider store={store}>
+      <div className='text-center text-9xl text-red-500'>App</div>
+    </Provider>
   )
 }
 
---
+++ client/src/app/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit' // ConfigureStore returns an object to create the store
+
+export const store = configureStore({ // Create an instance from store.
+  reducer: {},
+})
