be615012d19a501e39bd030b7b178798fb65db21
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-20 15:15:02 -0300

    Fix: The Provider and store must be imported in the main.jsx file and wrap the App component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { store } from './app/store' // Import the store created
+import { Provider } from 'react-redux' // Import provider from redux
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
