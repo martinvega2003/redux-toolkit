873471c0db9c83ac3b70a7bc658d2edff205514e
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-21 14:29:50 -0300

    Import TaskForm and TaskList in App.jsx and render them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux' // useSelector is used to select values from the state, useDispatch is used to execute actions on the state
+import TaskForm from './components/TaskForm.jsx'
+import TasksList from './components/TasksList.jsx'
 
 const App = () => {
   const taskState = useSelector(state => state.tasks) // tasks is the name assigned to taskReducer in store.js
   console.log(taskState)
   return ( // Wrap the App with the redux Provider and pass the store as prop
-    <div className='text-center text-9xl text-red-500'>App</div>
+    <div className='flex flex-col justify-center items-center'>
+      <TaskForm />
+      <TasksList />
+    </div>
   )
 }
 
