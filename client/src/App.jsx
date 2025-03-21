e873989d2d99e61caa499e9be66ab8f5b103c6da
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-21 14:59:40 -0300

    Create Routes in App.jsx with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,19 @@ import { useSelector, useDispatch } from 'react-redux' // useSelector is used to
 import TaskForm from './components/TaskForm.jsx'
 import TasksList from './components/TasksList.jsx'
 
+//react-router-dom:
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 const App = () => {
   const taskState = useSelector(state => state.tasks) // tasks is the name assigned to taskReducer in store.js
   console.log(taskState)
   return ( // Wrap the App with the redux Provider and pass the store as prop
-    <div className='flex flex-col justify-center items-center'>
-      <TaskForm />
-      <TasksList />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TasksList />} />
+        <Route path='/create-task' element={<TaskForm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
