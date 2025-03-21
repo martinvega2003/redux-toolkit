93c1a8f8f03b18ff5b42a24b31f51e856e133c33
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-21 14:52:20 -0300

    add deleteTask reducer to taskSlice, which receives the task ID in the payload

--- client/src/features/tasks/taskSlice.js
+++ client/src/features/tasks/taskSlice.js
@@ -6,10 +6,16 @@ export const taskSlice = createSlice({
   reducers: { // Here we add functions to modify the initial state of this slice.
     addTask: (state, action) => { // state is the current state and action is the parameter, which is an object with keys: name and payload (Payload being the value)
       state.push(action.payload) // As state is the array, the action.payload (value) is pushed
+    },
+    deleteTask: (state, action) => { // action.payload is the id, action.type is the reducer executed (deleteTask)
+      const taskFound = state.find(task => task.id === action.payload)
+      if (taskFound) {
+        state.splice(state.indexOf(taskFound), 1)
+      }
     }
   }
 })
 
-export const {addTask} = taskSlice.actions // Export the reducers
+export const {addTask, deleteTask} = taskSlice.actions // Export the reducers
 
 export default taskSlice.reducer // We import the reducer of the slice by default.
