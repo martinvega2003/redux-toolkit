1ad9b4e7b8eb514f4ae54b7255b3dd1cb374441e
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-21 14:53:13 -0300

    add a button to task list items to execute the deleteTask reducer

--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteTask } from '../features/tasks/taskSlice'
 
 const TasksList = () => {
 
   const tasks = useSelector(state => state.tasks)
+  const dispatch = useDispatch()
+
+  const handleDelete = id => {
+    dispatch(deleteTask(id))
+  }
 
   return (
     <div>
@@ -12,6 +18,7 @@ const TasksList = () => {
           <div key={task.id}>
             <h3 className='text-5xl'>{task.title}</h3>
             <p className='text-3xl'>{task.description}</p>
+            <button onClick={() => handleDelete(task.id)}>Delete</button>
           </div>
         )
       })}
