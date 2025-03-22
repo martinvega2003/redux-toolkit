ec600524fb09bd00de4a1d96a6886bd537d0d230
martinvega2003 <josemartinvt2003@gmail.com>
2025-03-22 12:36:21 -0300

    Create and test a ThemeMode context to toggle between dark and light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,28 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux' // useSelector is used to select values from the state, useDispatch is used to execute actions on the state
 import TaskForm from './components/TaskForm.jsx'
 import TasksList from './components/TasksList.jsx'
+import Navbar from './components/Navbar.jsx'
+import { ThemeModeProvider } from './context/themeModeContext.jsx'
 
 //react-router-dom:
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   const taskState = useSelector(state => state.tasks) // tasks is the name assigned to taskReducer in store.js
   console.log(taskState)
   return ( // Wrap the App with the redux Provider and pass the store as prop
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<TasksList />} />
-        <Route path='/create-task' element={<TaskForm />} />
-        <Route path='/edit-task/:id' element={<TaskForm />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeModeProvider>
+      <div className="dark:bg-zinc-900 text-black dark:text-white h-screen">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<TasksList />} />
+            <Route path='/create-task' element={<TaskForm />} />
+            <Route path='/edit-task/:id' element={<TaskForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeModeProvider>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
+import { FaSun, FaMoon } from 'react-icons/fa'
+import { ThemeModeContext } from '../context/themeModeContext.jsx'
 
 const Navbar = () => {
+
+  const { darkMode, setDarkMode } = useContext(ThemeModeContext);
+
   return (
     <div className='fixed top-0 bg-white w-full py-4 flex justify-end items-center shadow-2xl'>
+      <button 
+        onClick={() => setDarkMode(!darkMode)} 
+        className='mx-2 text-zinc-900 hover:text-blue-600 text-4xl transition duration-300 cursor-pointer hover:scale-110'
+      >
+        {darkMode ? <FaMoon /> : <FaSun />}
+      </button>
+
       <Link to='/' className='py-2 px-4 mx-2 text-center border border-blue-400 bg-blue-400 hover:bg-transparent text-white hover:text-blue-400 rounded-2xl transition duration-300'>
         Tasks List
       </Link>
